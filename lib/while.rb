8d59c7b1d7883209d37ebdba56eb717f588c6c1d
Victoria Curl <victoriacurl9@gmail.com>
2019-12-15 21:49:58 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,6 @@
 def using_while
   levitation_force = 6
-  until levitation_force == 10 
-    puts "Wingardium Leviosa" 
-  counter += 1    
+  until levitation_force == 10
+    puts "Wingardium Leviosa"
+  counter += 1
 end
-
-
